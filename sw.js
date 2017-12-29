58a0ec0eb4e958b5da0224136ca6f64e84e60e13
dinesh <dinesh@dinesh-Lenovo-G570>
2017-12-30 02:40:11 +0530

    1. added event fetch

--- sw.js
+++ sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', function(event){
   console.log('[Service Worker] Activated');
   return self.clients.claim();
 })
+
+self.addEventListener('fetch', function(event){
+  console.log('[Service Worker] fetching');
+  event.respondWith(fetch(event.request));
+})
